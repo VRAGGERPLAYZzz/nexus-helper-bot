19b07c97092da6ab77269000400766494d26e0e7
VRAGGERPLAYZzz <lordxvragger@gmail.com>
2026-06-04 12:34:12 +0530

    Update bot-logic.js

--- bot-logic.js
+++ bot-logic.js
@@ -2,7 +2,7 @@ const mineflayer = require('mineflayer');
 const { randomChats } = require('./chats');
 
 const botOptions = {
-  host: 'LifeNexus.aternos.me',
+  host: 'dory.aternos.host',
   port: 61341,
   username: 'helper',
   version: '1.21.1' 
